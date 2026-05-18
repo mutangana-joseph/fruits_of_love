00b7e31c3c762d27db812836c74ca05b0bdd4dac
mutanganajoseph <josephmutangana65@gmail.com>
2026-05-18 18:42:28 +0200

    Designed the donate card and implemented the menu on mobiles also implemented the java scripts for handleing show and hide, menu/donatecard

--- scripts.js
+++ scripts.js
@@ -1,7 +1,21 @@
 const donate = document.getElementById('donate');
 const methods = document.getElementById('methods');
 const show_menu = document.getElementById('show-menu');
+const close_menu = document.getElementById('close-menu');
 const mobile_menu = document.getElementById('mobile-menu');
+
+//Show and hide Mobile menu function
+function showMenu(){
+
+    if (mobile_menu.style.display === 'none' ||mobile_menu.style.display === ''){
+        mobile_menu.style.display = 'block';
+    }
+    else{
+        mobile_menu.style.display = 'none';
+
+    }
+}
+
 
 
 //Show or Hide Donate methods
@@ -10,6 +24,7 @@ function showHide(){
     if(donate){
         if (methods.style.display === 'none' || methods.style.display === '') {
             methods.style.display = 'block';
+            mobile_menu.style.display = 'none';
         }
         else {
             methods.style.display = 'none';
@@ -20,13 +35,3 @@ function showHide(){
 }
 
 
-//Show mobile menu
-function showMenu(){
-
-    if (mobile_menu.style.display === 'none' || mobile_menu.style.display === '') {
-        mobile_menu.style.display = 'flex';
-    }
-    else {
-        mobile_menu.style.display = 'none';
-    }  
-}
